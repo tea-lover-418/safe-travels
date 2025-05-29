a13262342518614745b4ef4f14ca40834fd9c2e7
tea-lover-418 <stijndeligt@gmail.com>
2025-05-29 23:05:51 +0200

    feat: page metadata

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,6 +3,7 @@ import { Map } from "../components/Map";
 import { getDbClient } from "../db";
 import { locationCollection } from "../db/collections";
 import { Location } from "@safe-travels/models/location";
+import { Metadata } from "next";
 
 const getData = async () => {
   const client = await getDbClient();
@@ -27,3 +28,9 @@ export default async function Home() {
     </div>
   );
 }
+
+export const generateMetadata = async (): Promise<Metadata> => {
+  return {
+    title: "Safe Travels",
+  };
+};
