d158cb57656cd1ff9b39fae13ff8199f958ec6fe
tea-lover-418 <stijndeligt@gmail.com>
2025-06-02 23:49:31 +0200

    fix: somewhat improve permissions

--- apps/android/app/src/main/java/com/example/safetravels/ConfigForm.kt
+++ apps/android/app/src/main/java/com/example/safetravels/ConfigForm.kt
@@ -1,4 +1,4 @@
-package com.example.yourapp.ui
+package com.example.safetravels
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
--- apps/android/app/src/main/java/com/example/safetravels/MainActivity.kt
+++ apps/android/app/src/main/java/com/example/safetravels/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.safetravels
 
 import android.Manifest
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
@@ -21,7 +23,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import com.example.yourapp.ui.ConfigForm
 
 class MainActivity : ComponentActivity() {
 
@@ -29,25 +30,38 @@ class MainActivity : ComponentActivity() {
         arrayOf(
             Manifest.permission.ACCESS_FINE_LOCATION,
             Manifest.permission.ACCESS_COARSE_LOCATION,
-            Manifest.permission.ACCESS_BACKGROUND_LOCATION,
-            Manifest.permission.REQUEST_IGNORE_BATTERY_OPTIMIZATIONS
         )
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private val backgroundPermissions = arrayOf(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+
+    override fun onStart() {
+        super.onStart()
+
+        val apiLevel = Build.VERSION.SDK_INT
+        Log.d("DEVICE_API", "Device is running API level: $apiLevel")
 
         val permissionLauncher =
             registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
+                Log.d("INFO", "requesting permission")
                 schedulePeriodicLocationWork(this)
             }
 
         permissionLauncher.launch(locationPermissions)
+        permissionLauncher.launch(
+            backgroundPermissions
+        ) // TODO: doesn't get triggered now until re-render
 
-        setContent {
-            MaterialTheme {
-                AppWithTabs()
-            }
-        }
+        // TODO: Bring this to something in the config
+        // val intent = Intent(Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS)
+        // startActivity(intent)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Log.d("INFO", "we creating")
+
+        setContent { MaterialTheme { AppWithTabs() } }
     }
 
     @OptIn(ExperimentalMaterial3Api::class)
@@ -81,4 +95,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
