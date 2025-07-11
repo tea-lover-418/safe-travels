866c2848a8be381fdfa3910dee9cdd6ff03bc71c
tea-lover-418 <stijndeligt@gmail.com>
2025-07-11 10:32:48 +0200

    fix: remove docusaurus v4 because that shit is broken

--- apps/docs/docusaurus.config.ts
+++ apps/docs/docusaurus.config.ts
@@ -9,28 +9,15 @@ const config: Config = {
   tagline: "The privacy friendly travel diary, for self hosting",
   favicon: "img/favicon.ico",
 
-  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
-  future: {
-    v4: true, // Improve compatibility with the upcoming Docusaurus v4
-  },
-
   // Set the production url of your site here
   url: "https://safe-travels.app",
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: "safe-travels", // Usually your GitHub org/user name.
-  projectName: "safe-travels", // Usually your repo name.
+  organizationName: "safe-travels",
+  projectName: "safe-travels",
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: "en",
     locales: ["en"],
@@ -58,7 +45,6 @@ const config: Config = {
   ],
 
   themeConfig: {
-    // Replace with your project's social card
     image: "img/icon-2.png",
     navbar: {
       title: "Safe Travels",
