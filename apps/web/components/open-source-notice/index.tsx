c6e9c73093bb34d8f15bbbb5e9b18a3a43764de6
tea-lover-418 <stijndeligt@gmail.com>
2025-07-12 12:44:43 +0200

    chore: update links to websites in open source notice

--- apps/web/components/open-source-notice/index.tsx
+++ apps/web/components/open-source-notice/index.tsx
@@ -6,14 +6,26 @@ export const OpenSourceNotice = () => {
       <div className={styles.ossContainer}>
         <h3 className={styles.builtWith}>
           Built with{" "}
-          <a href="https://github.com/tea-lover-418/safe-travels">
+          <a href="https://safe-travels.app" target="_blank" rel="noreferrer">
             SafeTravels
           </a>
         </h3>
       </div>
       <div className={styles.ossContainer}>
-        <a href="https://github.com/tea-lover-418/safe-travels">github</a>
-        <a href="https://github.com/tea-lover-418/safe-travels">docs</a>
+        <a
+          href="https://github.com/tea-lover-418/safe-travels"
+          target="_blank"
+          rel="noreferrer"
+        >
+          github
+        </a>
+        <a
+          href="https://safe-travels.app/docs/intro"
+          target="_blank"
+          rel="noreferrer"
+        >
+          docs
+        </a>
         <p>© 2025</p>
       </div>
     </div>
