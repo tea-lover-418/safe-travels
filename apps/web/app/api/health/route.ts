ecf6ac6b95030a3e1766db568b449994b38ba33d
tea-lover-418 <stijndeligt@gmail.com>
2025-07-25 14:10:57 +0200

    feat: health endpoint

---
+++ apps/web/app/api/health/route.ts
@@ -0,0 +1,13 @@
+import { isAuthorized } from "../../../utils/auth";
+
+export async function POST(request: Request) {
+  const token = request.headers.get("Authorization");
+
+  if (!isAuthorized(token)) {
+    return new Response(undefined, {
+      status: 401,
+    });
+  }
+
+  return new Response(undefined, { status: 200 });
+}
