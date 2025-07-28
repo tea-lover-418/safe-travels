84ab0373272ea0ff23ed64882c18efd72d143460
tea-lover-418 <stijndeligt@gmail.com>
2025-07-28 16:31:27 +0200

    fix: bring back middleware thanks to tsconfig solution

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,38 +1,31 @@
-/** For some reason, adding an import to next/server breaks a lot of things down the line.
- *  I feel it must be a Next.js internal issue at this point, but kinda need this middleware fam.
- *  Maybe I'll just ditch it and move to NextAuth instead.
- */
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import { serverConfig } from './config';
 
-// import { NextResponse } from 'next/server';
-// import type { NextRequest } from 'next/server';
-// import { serverConfig } from './config';
+export default function middleware(request: NextRequest) {
+  const password = serverConfig.security.publicPassword;
+  if (!password) {
+    return NextResponse.next();
+  }
 
-// export default function middleware(request: NextRequest) {
-//   const password = serverConfig.security.publicPassword;
-//   if (!password) {
-//     return NextResponse.next();
-//   }
+  if (request.nextUrl.pathname.includes('/login')) {
+    return NextResponse.next();
+  }
 
-//   if (request.nextUrl.pathname.includes('/login')) {
-//     return NextResponse.next();
-//   }
+  const cookie = request.cookies.get('auth')?.value;
+  const authorized = cookie === password;
 
-//   const cookie = request.cookies.get('auth')?.value;
-//   const authorized = cookie === password;
+  if (!authorized) {
+    const loginUrl = new URL('/login', request.url);
+    return NextResponse.redirect(loginUrl);
+  }
 
-//   if (!authorized) {
-//     const loginUrl = new URL('/login', request.url);
-//     return NextResponse.redirect(loginUrl);
-//   }
+  return NextResponse.next();
+}
 
-//   return NextResponse.next();
-// }
-
-// /** This is a verbose allowlist, be sure to add any other routes here that need protection.
-//  * Do not pattern match as you will likely include many assets.
-//  * */
-// export const config = {
-//   matcher: ['/'],
-// };
-
-export default () => {};
+/** This is a verbose allowlist, be sure to add any other routes here that need protection.
+ * Do not pattern match as you will likely include many assets.
+ * */
+export const config = {
+  matcher: ['/'],
+};
